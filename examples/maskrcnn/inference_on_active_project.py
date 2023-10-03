44793c21002ac2fa5a24531152d7042b6f9dce00
Gorkem Polat <gorkem@encord.com>
2023-10-03 09:22:23 +0100

    update to formal iteration

--- examples/maskrcnn/inference_on_active_project.py
+++ examples/maskrcnn/inference_on_active_project.py
@@ -1,16 +1,16 @@
 import json
 from pathlib import Path
-import os
 import pickle
 import cv2
 import torch
-from encord_active.db.models import ProjectDataUnitMetadata, get_engine, Project
+from encord_active.db.models import  get_engine
 from encord_active.lib.db.predictions import BoundingBox, Format, ObjectDetection, Prediction
-from PIL import Image
-from sqlmodel import Session, select
 from utils.model_libs import get_model_instance_segmentation
 from utils.provider import get_config, get_transform
 import numpy as np
+from encord_active.public.active_project import ActiveContext
+import uuid
+from tqdm import tqdm
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 params = get_config("config.ini")
@@ -31,57 +31,59 @@ model.to(device)
 
 img_transformer = get_transform(train=False)
 
-with Session(engine) as sess:
-    project = sess.exec(
-        select(Project).where(Project.project_hash == params.encord.project_hash)
-    ).first()
-    project_ontology = project.ontology
+active_context = ActiveContext(path.parent)
+active_project = active_context.get_project(uuid.UUID(params.encord.project_hash))
+all_active_projects = active_context.list_projects()
+project_ontology = active_project.ontology.to_dict()
 
 model.eval()
 with torch.no_grad():
-    with Session(engine) as sess:
-        all_du = sess.exec(
-            select(ProjectDataUnitMetadata).where(ProjectDataUnitMetadata.project_hash == params.encord.project_hash)
-        ).fetchall()
-        for du in all_du:
-            img = img_transformer(Image.open(du["data_uri"]).convert("RGB"))
-            prediction = model([img.to(device)])
+    for counter, du in enumerate(tqdm(active_project.iter_frames())):
+        if counter == 20:
+            break
 
-            scores_filter = prediction[0]["scores"] > params.inference.confidence_threshold
-            masks = prediction[0]["masks"][scores_filter].detach().cpu().numpy()
-            labels = prediction[0]["labels"][scores_filter].detach().cpu().numpy()
-            scores = prediction[0]["scores"][scores_filter].detach().cpu().numpy()
+        image = du.image
+        img, _ = img_transformer(image, None)
+        prediction = model([img.to(device)])
 
-            for ma, la, sc in zip(masks, labels, scores):
-                contours, _ = cv2.findContours(
-                    (ma[0] > 0.5).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
-                )
+        scores_filter = prediction[0]["scores"] > params.inference.confidence_threshold
+        masks = prediction[0]["masks"][scores_filter].detach().cpu().numpy()
+        labels = prediction[0]["labels"][scores_filter].detach().cpu().numpy()
+        scores = prediction[0]["scores"][scores_filter].detach().cpu().numpy()
 
-                for contour in contours:
-                    contour = contour.reshape(contour.shape[0], 2) / np.array([[ma.shape[2], ma.shape[1]]])
-                    if project_ontology["objects"][la.item() - 1]['shape'] == 'bounding_box':
-                        (x, y, w, h) = cv2.boundingRect(contour)
-                        obj = ObjectDetection(
-                            format=Format.BOUNDING_BOX,
-                            data=BoundingBox(x=x, y=y, w=w, h=h),
-                            feature_hash=project_ontology["objects"][la.item() - 1]["featureNodeHash"],
-                        )
-                    elif project_ontology["objects"][la.item() - 1]['shape'] == 'polygon':
-                        obj = ObjectDetection(
-                            format=Format.POLYGON,
-                            data=contour,
-                            feature_hash=project_ontology["objects"][la.item() - 1]["featureNodeHash"],
-                        )
-                    else:
-                        continue
+        for ma, la, sc in zip(masks, labels, scores):
+            contours, _ = cv2.findContours(
+                (ma[0] > 0.5).astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
+            )
 
-                    prediction = Prediction(
-                        data_hash=str(du.data_hash),
-                        frame=du.frame,
-                        confidence=sc.item(),
-                        object=obj,
+            for contour in contours:
+                if project_ontology["objects"][la.item() - 1]['shape'] == 'bounding_box':
+                    (x, y, w, h) = cv2.boundingRect(contour)
+                    obj = ObjectDetection(
+                        format=Format.BOUNDING_BOX,
+                        data=BoundingBox(x=x / ma.shape[2], y=y / ma.shape[1], w=w / ma.shape[2], h=h / ma.shape[1]),
+                        feature_hash=project_ontology["objects"][la.item() - 1]["featureNodeHash"],
                     )
-                    predictions_to_store.append(prediction)
+                elif project_ontology["objects"][la.item() - 1]['shape'] == 'polygon':
+                    contour = contour.reshape(contour.shape[0], 2) / np.array([[ma.shape[2], ma.shape[1]]])
+                    obj = ObjectDetection(
+                        format=Format.POLYGON,
+                        data=contour,
+                        feature_hash=project_ontology["objects"][la.item() - 1]["featureNodeHash"],
+                    )
+                else:
+                    continue
 
-with open(Path(params.inference.target_data_folder).parent / f"predictions_{params.inference.wandb_id}.pkl", "wb") as f:
+                prediction = Prediction(
+                    data_hash=str(du.data_hash),
+                    frame=du.frame,
+                    confidence=sc.item(),
+                    object=obj,
+                )
+                predictions_to_store.append(prediction)
+
+
+with open(Path(params.encord.data_folder).parent / f"predictions_{params.inference.wandb_id}.pkl", "wb") as f:
     pickle.dump(predictions_to_store, f)
+
+
